496f9a210f7c297f6ac0954b0e1f219c895ab1e6
Ruben9911 <ruben.fortes-marques@iesdoctorbalmis.com>
2021-11-03 16:32:10 +0100

    Agregar archivos de proyecto.

---
+++ MemeMaker/YesNoBorder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MemeMaker
+{
+    class YesNoBorder : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isChecked = (bool)value;
+
+            if (isChecked) return 3;
+            else return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
